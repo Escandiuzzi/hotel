4925e0c74b8cc5f020b82bfc12c61d26b3fe7304
vergonha <shirosayuya@gmail.com>
2025-10-20 14:16:42 -0300

    fix: cors

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3333;
 const allowedOrigin = process.env.API_URL || 'http://localhost:3000';
 
-app.use(cors({ origin: allowedOrigin }));
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
